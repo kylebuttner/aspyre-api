fe24dc0096ba30b066a4af08f1ee540bf581df7d
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-05-03 16:43:28 +0100

    added post request to create new goal resource

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,4 +4,15 @@ class GoalsController < ApplicationController
     @goals = Goal.all
     render json: @goals
   end
+
+  def create
+    Goal.create(name: params[:name])
+    render json:{}, status: :created  
+  end
+
+private
+
+  def goals_params
+    params.require(:goal).permit(:name)
+  end
 end
--- spec/features/goal_feature_spec.rb
+++ spec/features/goal_feature_spec.rb
@@ -9,12 +9,29 @@ describe 'Goals', type: :request do
     end
 
     it 'returns all goals' do
-      get '/goals', {}, { 'Accept': 'application/json' }
+      get('/goals', {}, { 'Accept': 'application/json' })
 
       expect(response.status).to eq 200
 
       expect(parsed_response[0]['name']).to eq("FactoryGoalName")
       expect(parsed_response.length).to eq(3)
+    end
+  end
+  
+  describe 'POST/movies' do 
+    it 'creates a goal' do 
+    
+      goal_params = {name: "Learn piano"}.to_json
+
+      request_headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+      }
+
+      post("/goals", goal_params, request_headers)
+
+      expect(response.status).to eq 201
+      expect(Goal.first.name).to eq("Learn piano")
     end
   end
 end
